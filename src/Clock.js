880a2cac246c7e2d29cf1abdc3ef5601810ffea0
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2022-11-04 14:21:34 -0400

    commit tree b8a8ba038d06548951048ecd9da453366363ec3b

--- src/Clock.js
+++ src/Clock.js
@@ -4,9 +4,14 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
-      setTime(new Date());
+    const timerID = setInterval(() => {
+      setTime(new Date()); 
     }, 1000);
+
+    //returning a cleanup function that
+    return function cleanup() {
+      clearInterval(timerID);
+    };
   }, []);
 
   return <div>{time.toString()}</div>;
